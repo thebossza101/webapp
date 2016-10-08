9e99451c5e29faf21566dd65fefa97e7520f12f6
thebossza101 <rujiroj2537@gmail.com>
2016-10-09 06:14:02 +0700

    initial project
    set up Angular2 and bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule,
+    routing
   ],
   providers: [],
+  entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,21 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+    {
+  path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+},
+    { path: 'home', component: HomeComponent }, 
+
+];
+
+// เรา export ตัวแปรประเภทค่าคงที่ (const) ชื่อ routing ออกไป
+// routing นี้เป็นผลลัพธ์จากการเรียก RouterModule.forRoot(appRoutes)
+// โดย routing ของเราเป็น ModuleWithProviders
+// เพื่อนๆคนไหนไม่เข้าใจว่าทำไมเราต้องเขียน routing: ModuleWithProviders
+// แนะนำให้อ่าน ชุดบทความสอนใช้งาน TypeScript ที่ https://www.babelcoder.com/blog/series/typescript ครับ
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes,{ useHash: true });
